b89153f5716a59fe17e0661caa4844e35e82907f
FlappyIce <flappyice73@outlook.com>
2021-02-21 01:09:26 -0500

    fixed user problem

--- src/commands/core/user/user.ts
+++ src/commands/core/user/user.ts
@@ -1,14 +1,14 @@
-import { KBotify } from "../../../utils/kbotify";
+import { KBotify } from '../../../utils/kbotify';
 
 export class User {
     id: string = '';
-    nickname: string = '';
-    identifyNum: string
-    bot: KBotify
+    username: string = '';
+    identifyNum: string;
+    bot: KBotify;
     constructor(userObject: any, bot: KBotify) {
         this.id = userObject.id;
-        this.nickname = userObject.nickname;
-        this.identifyNum = userObject.identify_num
-        this.bot = bot
+        this.username = userObject.username ?? userObject.nickname;
+        this.identifyNum = userObject.identify_num;
+        this.bot = bot;
     }
 }
