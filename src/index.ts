444be00c8bb7b1ab9b0bba139e86359fd76573a2
FlappyIce <flappyice73@outlook.com>
2021-03-04 04:31:13 -0500

    added export

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,8 @@ export { MenuCommand } from './core/menu.command';
 export { SendOptions } from './core/msg.types';
 
 export { KBotify } from './core/kbotify';
+
+export * from './core/user';
+export * from './core/message';
+export * from './core/kbotify';
+export * from './core/channel';
