57d393c1978a315f291e902b4be01c352453ff5d
FlappyIce <flappyice73@outlook.com>
2021-06-24 09:07:59 -0400

    merged conflicts

--- src/commands/echo/echo.card.app.ts
+++ src/commands/echo/echo.card.app.ts
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import { AppCommand, AppFunc, BaseSession } from '../..';
 
 class EchoCard extends AppCommand {
-=======
-import { BaseSession } from 'commands/core/session';
-import { MessageType } from 'kaiheila-bot-root/dist/types';
-import { AppCommand } from '../core/app.command';
-import { AppCommandFunc, BaseData } from '../core/app.types';
-import { ResultTypes } from '../core/types';
-
-class EchoCard extends AppCommand<BaseSession> {
->>>>>>> origin/main
     code = 'card';
     trigger = 'card';
     help = '`.echo card 内容`';
     intro = '复读你所说的文字, 并用CardMessage格式返回。';
-<<<<<<< HEAD
     func: AppFunc<BaseSession> = async (session) => {
         const msg: string = ''.concat(...session.args).replace('\\n', '');
         // console.log(msg)
@@ -77,17 +66,6 @@ class EchoCard extends AppCommand<BaseSession> {
                 ],
             },
         ];
-=======
-    func: AppCommandFunc<BaseSession> = async (session) => {
-        const msg: string = ''.concat(...session.args).replace('\\n', '')
-        // console.log(msg)
-        return this.msgSender.send(
-            msg,
-            session,
-            ResultTypes.SUCCESS,
-            { reply: true, msgType: 10 }
-        );
->>>>>>> origin/main
     };
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
 export * from './core/session';
-=======
-export { BaseSession } from './commands/core/session';
-export {
-    AppCommandFunc,
-    BaseData,
-    FuncResult,
-    AppCommandParams,
-} from './commands/core/app.types';
-export { MsgSender as AppMsgSender } from './commands/core/msg.sender';
-export { ResultTypes } from './commands/core/types';
-export { AppCommand } from './commands/core/app.command';
-export { MenuCommand } from './commands/core/menu.command';
-export { SendOptions } from './commands/core/msg.types';
->>>>>>> origin/main
 
 export { ResultTypes } from './core/types';
 export * from './core/command';
