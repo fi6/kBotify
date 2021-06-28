fcf211369b2ee3ad81a08339e0f548b97ba8ff34
FlappyIce <flappyice73@outlook.com>
2021-06-27 22:30:01 -0400

    added export for card

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './core/session';
 
 export { ResultTypes } from './core/types';
 export * from './core/command';
+export * from './core/card';
 export { SendOptions } from './core/msg.types';
 
 export { KBotify } from './core/kbotify';
